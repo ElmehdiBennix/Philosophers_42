5a15763ccfac6ae44076e984ba518c8c16afecae
El Mehdi Bennix <ebennix@e1r9p9.1337.ma>
2023-05-21 15:14:25 +0100

    MakefileAutoPush

--- Mandatory/inc/philo.h
+++ Mandatory/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 20:12:33 by ebennix           #+#    #+#             */
-/*   Updated: 2023/05/19 17:02:57 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/05/19 21:50:43 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,25 @@
 # include <sys/time.h>
 # include <pthread.h>
 
+// they can do only one thing at a time
+// number of forks is same as philos
+// when they eat they put back the fork and sleep
+// to eat u need 2 forks
+// all should eat cant starve and die
+// cant know if philo is about to die
+// dont speak with the other philo
 
 typedef struct philo{
-    
+    unsigned int    n_philo;
+    unsigned int    death_time;
+    unsigned int    eat_time;
+    unsigned int    time_to_sleep;
+    unsigned int    number_of_time_philo_eats;
 
 }               data;
 
-
+void parse(int ac, char **av);
+int	    ft_isdigit(char c);
 void	exit_msg(char *msg, char *color, int erno);
-void    parse(char **av);
-
-
 
 #endif
--- Mandatory/philo.c
+++ Mandatory/philo.c
@@ -6,11 +6,11 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 20:13:14 by ebennix           #+#    #+#             */
-/*   Updated: 2023/05/19 17:02:13 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/05/19 21:29:14 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#include "inc/philo.h"
 
 int philo(int ac, char **av)
 {
@@ -19,7 +19,7 @@ int philo(int ac, char **av)
     philo = malloc(sizeof(data));
     if (!philo)
         exit_msg("Memory problem !!", RED, 1);
-    parse(++av);
+    parse(ac ,av);
 
     return (0);
 }
@@ -28,8 +28,8 @@ int main (int ac, char **av)
 {
     int err;
 
-    if (ac != 4 || ac != 5)
+    if (ac < 5 || ac > 6)
         exit_msg("4 or 5 arguments are allowed", RED, 1);
-    err = philo(ac, av);
+    err = philo(ac, ++av);
     return (err);
 }
