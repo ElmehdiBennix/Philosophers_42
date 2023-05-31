99206af1dc54e0f67447d21e18e50dcd6be33b92
ElmehdiBennix <bennixmehdi@gmail.com>
2023-05-31 04:57:32 +0100

    MakefileAutoPush

--- Mandatory/life_cycle/philo_cycle.c
+++ Mandatory/life_cycle/philo_cycle.c
@@ -6,35 +6,54 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 00:35:25 by ebennix           #+#    #+#             */
-/*   Updated: 2023/05/30 05:35:05 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/05/31 04:56:37 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-// void get_time()
-// {
-    
-// }
-
-void dinner_time(t_list *philo , unsigned int time)
+unsigned long get_time(long start_time)
 {
-    pthread_mutex_lock(&philo->fork);
-    pthread_mutex_lock(&philo->next->fork);
-    printf("philo is eating");
-    sleep(time);
-    //get time 
-    pthread_mutex_unlock(&philo->fork);
-    pthread_mutex_unlock(&philo->next->fork);
+    struct timeval time;
+    unsigned long s;
+    unsigned long m;
+    gettimeofday(&time, NULL);
+    
+    s = time.tv_sec * 1000;
+    m = time.tv_usec / 1000;
+    return ((s+m) - start_time);
 }
+
+// void dinner_time(t_list *philo , unsigned int time)
+// {
+//     pthread_mutex_lock(&philo->fork);
+//     printf("%d ms philo = %d has taken a fork", );
+//     pthread_mutex_lock(&philo->next->fork);
+//     printf("%d ms philo = %d has taken a fork", );
+//     printf("%d ms philo = %d is eating");
+//     sleep(time);
+//     //get time 
+//     // incremet ate
+//     pthread_mutex_unlock(&philo->fork);
+//     pthread_mutex_unlock(&philo->next->fork);
+// }
 
 void    philo_cycle(t_data *var)
 {
-    while(true)
-    {
-        dinner_time(var->philosophers, var->eating_time);
-        printf("philo is sleeping");
-        sleep(var->sleeping_time);
-        printf("philo is thinking");
-    }
+    (void)var;
+    struct timeval time;
+    gettimeofday(&time, NULL);
+
+    sleep(5);
+    printf("philo started = %lu \n", get_time((time.tv_sec*1000)+(time.tv_usec/1000)));
+    sleep(5);
+    printf("philo finished\n");
+
+    // while(true)
+    // {
+    //     dinner_time(var->philosophers, var->eating_time);
+    //     printf("%d ms philo = %d is sleeping");
+    //     sleep(var->sleeping_time);
+    //     printf("%d ms philo = %d is thinking");
+    // }
 }
