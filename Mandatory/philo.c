251b4c5c234f7ef5fd3b1c3c0d03d97ec9ec1c19
El Mehdi Bennix <ebennix@e1r9p8.1337.ma>
2023-06-01 14:54:24 +0100

    MakefileAutoPush

--- Mandatory/philo.c
+++ Mandatory/philo.c
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 20:13:14 by ebennix           #+#    #+#             */
-/*   Updated: 2023/06/01 14:28:07 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/06/01 14:32:26 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,29 @@
 
 int philo(int ac, char **av)
 {
-    t_data var;
+    t_data *var;
     unsigned int i;
 
+    var = malloc(sizeof(t_data));
+
     i = 0;
-    parse(ac ,av, &var);
-    init_philos(&var);
-    while (i < var.numb_of_philos)
+    parse(ac ,av, var);
+    init_philos(var);
+    while (i < var->numb_of_philos)
     {
-        if(pthread_create(&var.philosophers->philo, NULL ,(void *)philo_cycle, var.philosophers) != 0)
+        if(pthread_create(&var->philosophers->philo, NULL ,(void *)philo_cycle, var->philosophers) != 0)
             exit_msg("error",RED,1);
-        printf("%d id has been created \n", var.philosophers->id);
-        var.philosophers = var.philosophers->next;
+        printf("%d id has been created \n", var->philosophers->id);
+        var->philosophers = var->philosophers->next;
         i++;
     }
     i = 0;
-    while (i < var.numb_of_philos)
+    while (i < var->numb_of_philos)
     {
-        if(pthread_join(var.philosophers->philo, NULL) != 0)
+        if(pthread_join(var->philosophers->philo, NULL) != 0)
             exit_msg("error",RED,1);
-        printf("%d id has been joined \n", var.philosophers->id);
-        var.philosophers = var.philosophers->next;
+        printf("%d id has been joined \n", var->philosophers->id);
+        var->philosophers = var->philosophers->next;
         i++;
     }
     //destroy mutex after the use;
