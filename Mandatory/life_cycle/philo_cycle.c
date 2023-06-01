fe2e99751b789cc96d9d42ef99d218b84cf2adca
ElmehdiBennix <bennixmehdi@gmail.com>
2023-06-01 14:54:24 +0100

    MakefileAutoPush

--- Mandatory/life_cycle/philo_cycle.c
+++ Mandatory/life_cycle/philo_cycle.c
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 00:35:25 by ebennix           #+#    #+#             */
-/*   Updated: 2023/06/01 14:29:21 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/06/01 14:52:46 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,36 +24,38 @@ unsigned long get_time(long start_time)
     return ((s + m) - start_time);
 }
 
-// void dinner_time(t_list *philo , unsigned int time)
-// {
-//     pthread_mutex_lock(&philo->fork);
-//     printf("%d ms philo = %d has taken a fork", );
-//     pthread_mutex_lock(&philo->next->fork);
-//     printf("%d ms philo = %d has taken a fork", );
-//     printf("%d ms philo = %d is eating");
-//     sleep(time);
-//     //get time 
-//     // incremet ate
-//     pthread_mutex_unlock(&philo->fork);
-//     pthread_mutex_unlock(&philo->next->fork);
-// }
+void dinner_time(t_list *philo , long time)
+{
+    pthread_mutex_lock(&philo->fork);
+    pthread_mutex_lock(&philo->next->fork);
+    printf("%lu ms philo = %d has taken a fork\n",get_time(time), philo->id);
+    printf("%lu ms philo = %d has taken a fork\n",get_time(time), philo->id);
+    printf("%lu ms philo = %d is eating\n", get_time(time), philo->id);
+    usleep(philo->var->eating_time);
+    philo->last_eat = get_time(time);
+    philo->n_eat++;
+    pthread_mutex_unlock(&philo->fork);
+    pthread_mutex_unlock(&philo->next->fork);
+}
 
 void    philo_cycle(t_list *philo)
 {
     unsigned long time = get_time(0);
 
-    printf("philo id = %d\n",philo->id);
+    // printf("philo id = %d\n",philo->id);
     // sleep(5);
     // printf("philo started = %lu \n", get_time(bruh));
     // sleep(5);
     // printf("philo started = %lu \n", get_time(bruh));
     // printf("philo finished\n");
 
-    // while(true)
-    // {
-    //     dinner_time(var->philosophers, var->eating_time);
-    //     printf("%d ms philo = %d is sleeping");
-    //     sleep(var->sleeping_time);
-    //     printf("%d ms philo = %d is thinking");
-    // }
+    while(true)
+    {
+        dinner_time(philo, time);
+        printf("%lu ms philo = %d is sleeping\n",get_time(time), philo->id);
+        usleep(philo->var->sleeping_time);
+        printf("%lu ms philo = %d is thinking\n",get_time(time), philo->id);
+    }
 }
+
+// make file problem
