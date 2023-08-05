cfe124bc40780201b24bd4415b9ba9d90d9900e0
ElmehdiBennix <bennixmehdi@gmail.com>
2023-08-05 20:31:35 +0100

    MakefileAutoPush

--- Mandatory/life_cycle/philo_cycle.c
+++ Mandatory/life_cycle/philo_cycle.c
@@ -6,43 +6,86 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 00:35:25 by ebennix           #+#    #+#             */
-/*   Updated: 2023/08/01 15:24:19 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/08/04 03:03:53 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-static void    dinner_time(t_philo *philo)
+void	ft_usleep(long time_in_ms)
+{
+	long	start_time;
+
+	start_time = get_time(0);
+	while (get_time(start_time) < time_in_ms)
+		usleep(100);
+}
+
+int    livelihood(t_philo *philo) // t_data *var
+{
+    printf("id = %d this is the last meal %lld > death time %d\n" ,philo->id,get_time(philo->last_meal) , philo->var->death_t );
+    if (get_time(philo->last_meal) > philo->var->death_t)
+    {
+		pthread_mutex_lock(&philo->var->print);
+        printf("%llu ms philo = %d has died\n",get_time(philo->var->start_clock), philo->id);
+		pthread_mutex_lock(&philo->var->death);
+		philo->var->stop = TRUE;
+        // pthread_mutex_unlock(&philo->var->death);
+        return (philo->var->stop);
+        // return TRUE; //stop sim once death
+    }
+    return (philo->var->stop);
+
+	//free data and loop over all philos 
+}
+
+int     lock_fork(t_philo *philo)
 {
     pthread_mutex_lock(&(philo->l_fork));
     pthread_mutex_lock(philo->r_fork);
-    pthread_mutex_lock(&philo->var->print);
-    printf("%llu ms philo = %d has taken a fork\n",get_time(philo->var->start_clock), philo->id);
-    printf("%llu ms philo = %d has taken a fork\n",get_time(philo->var->start_clock), philo->id);
-    printf("%llu ms philo = %d is eating\n", get_time(philo->var->start_clock), philo->id);
-    pthread_mutex_unlock(&philo->var->print);
-    philo->last_meal = get_time(0);
-    usleep(philo->var->eating_t * 1000);
-    if (philo->var->eating_reps != -1)
-        philo->meals_n++;
+    return (0);
+}
+int     unlock_fork(t_philo *philo)
+{
     pthread_mutex_unlock(&(philo->l_fork));
     pthread_mutex_unlock(philo->r_fork);
+    return (0);
 }
+
+# define take "has taken a fork"
+# define eat "is eating"
+# define sleep "is sleeping"
+# define think "is thinking"
+
+int     print_msg(t_philo *philo , char *action)
+{
+    pthread_mutex_lock(&(philo->var->print)); // problems dead lock for some reason
+    printf("%llu ms philo = %d %s\n",get_time(philo->var->start_clock), philo->id ,action);
+    pthread_mutex_unlock(&(philo->var->print));
+    return (0);
+}
+
 
 void    *philo_cycle(t_philo *philo)
 {
+    philo->last_meal = get_time(0);
     if (philo->id % 2 == 0)
-        usleep(50);
+        ft_usleep(50);
     while(1)
     {
-        dinner_time(philo);
-        pthread_mutex_lock(&philo->var->print);
-        printf("%llu ms philo = %d is sleeping\n", get_time(philo->var->start_clock), philo->id);
-        pthread_mutex_unlock(&philo->var->print);
-        usleep(philo->var->sleeping_t * 1000);
-        pthread_mutex_lock(&philo->var->print);
-        printf("%llu ms philo = %d is thinking\n",get_time(philo->var->start_clock), philo->id);
-        pthread_mutex_unlock(&philo->var->print);
+        lock_fork(philo);
+        print_msg(philo,take);
+        print_msg(philo,take);
+        print_msg(philo,eat);
+        ft_usleep(philo->var->eating_t);
+        philo->last_meal = get_time(0);
+        if (philo->var->eating_reps != -1)
+            philo->meals_n++;
+        unlock_fork(philo);
+        print_msg(philo,sleep);
+        ft_usleep(philo->var->sleeping_t);
+        print_msg(philo,think);
+        livelihood(philo);
     }
 }
 
--- Mandatory/parse/init_philos.c
+++ Mandatory/parse/init_philos.c
@@ -6,43 +6,33 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 18:17:20 by ebennix           #+#    #+#             */
-/*   Updated: 2023/08/01 15:36:48 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/08/04 02:56:41 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../inc/philo.h"
-
-int    livelihood(t_philo *philo) // t_data *var
-{
-    if (get_time(0) - philo->last_meal >= philo->var->death_t)
-    {
-		pthread_lock(philo->var->print);
-        printf("%llu ms philo = %d has died\n",get_time(philo->var->start_clock), philo->id);
-        // return TRUE; //stop sim once death
-		pthread_lock(philo->var->death);
-		philo->var->stop = TRUE;
-		pthread_unlock(philo->var->death);
-    }
-	//free data and loop over all philos 
-}
 
 int	init_philo(t_data *var)
 {
 	unsigned int i;
 	
 	var->start_clock = get_time(0);
+	var->stop = false;
 	i = -1;
 	var->philos = malloc(sizeof(t_philo) * var->n_philos);
 	if (!var->philos)
 		return (2);
 	while(++i < var->n_philos)
 	{
-		var->philos[i].id = i;
+		var->philos[i].id = i + 1;
 		var->philos[i].var = var;
 		var->philos[i].meals_n = 0;
-		var->philos[i].last_meal = get_time(0);
 		var->philos[i].r_fork = NULL;
 		// stops var should be false
+		if (pthread_mutex_init(&var->print, NULL) != 0)
+			return (2);
+		if (pthread_mutex_init(&var->death, NULL) != 0)
+			return (2);
 		if (pthread_mutex_init(&var->philos[i].l_fork, NULL) != 0)
 			return (2);
 		if (i != var->n_philos - 1)
@@ -54,8 +44,11 @@ int	init_philo(t_data *var)
 	}
 	i = -1;
 	while (++i < var->n_philos)
+	{
 		if (pthread_join(var->philos[i].p_thread, NULL) != 0)
 			return (2);
-	// add death checker hir 
+	}
+	// add death checker hir
+	// livelihood(var->philos);
 	return (0);
 }
